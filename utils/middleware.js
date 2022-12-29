ca058efddd1409e8408b02c26b7cf0055141143c
ongyongen <ongyongen2021@gmail.com>
2022-12-29 10:51:46 +0800

    edit error handling middleware

--- utils/middleware.js
+++ utils/middleware.js
@@ -18,11 +18,10 @@ const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id or input fields' })
-  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: 'malformatted id or input fields' })
+  } else {
     return response.status(400).json({ error: error.message })
   }
-  next(error)
 }
 
 module.exports = {
